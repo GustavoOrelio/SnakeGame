6c3b4a24aa40743f5e80b0ec1a750d6597432bae
Gustavo Orelio <oreliogustavo@gmail.com>
2023-08-29 22:07:48 -0300

    Arrumando os sprites da cobra

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ class Snake:
         self.load_sprites(sprite_paths, size)
         self.body = [SnakeSegment(self.sprites['head'], position, size, pygame.K_RIGHT)]
         self.direction = pygame.K_RIGHT
+        self.should_grow = False
 
     def load_sprites(self, sprite_paths, size):
         self.sprites = {}
@@ -62,6 +63,10 @@ class Snake:
         else:
             return
 
+        if self.should_grow:
+            self.grow()
+            self.should_grow = False
+
         # Cria um novo segmento de cabeça e o adiciona ao corpo
         new_head = SnakeSegment(self.sprites['head'], new_pos, self.sprite_size, new_direction)
         self.body.insert(0, new_head)
@@ -75,9 +80,27 @@ class Snake:
         self.body.append(SnakeSegment(self.sprites['cauda'], tail, self.sprite_size, tail_direction))
 
     def draw(self, window, size, rows):
+        curve_added = False
         for i in range(len(self.body)):
             segment = self.body[i]
             rotation_angle = 0
+            sprite_type = 'body'  # Assume-se que o segmento é do corpo por padrão
+
+            if i == 0:
+                sprite_type = 'head'
+            elif i == len(self.body) - 1:
+                sprite_type = 'cauda'
+            else:
+                prev_direction = self.body[i - 1].direction
+                next_direction = self.body[i + 1].direction
+                if prev_direction != next_direction and not curve_added and len(self.body) > 2:
+                    curve_added = True
+
+                    if (prev_direction == pygame.K_UP and next_direction == pygame.K_RIGHT) or \
+                            (prev_direction == pygame.K_LEFT and next_direction == pygame.K_DOWN):
+                        sprite_type = 'curve_right'
+                    else:
+                        sprite_type = 'curve_left'
 
             if segment.direction == pygame.K_UP:
                 rotation_angle = 0
@@ -88,33 +111,20 @@ class Snake:
             elif segment.direction == pygame.K_RIGHT:
                 rotation_angle = -90
 
-            # Verifique se é uma curva e se o corpo tem mais de 2 segmentos.
-            if 0 < i < len(self.body) - 1 and len(self.body) > 2:
-                prev_direction = self.body[i - 1].direction
-                next_direction = self.body[i + 1].direction
-
-                if prev_direction != next_direction:
-                    # Uma curva está acontecendo.
-                    if self.direction == prev_direction:
-                        # A cobra está virando em direção a next_direct
-                        # ion.
-                        rotated_sprite = pygame.transform.rotate(self.sprites['curve_right'], rotation_angle)
-                    else:
-                        # A cobra está virando em direção a prev_direction.
-                        rotated_sprite = pygame.transform.rotate(self.sprites['curve_left'], rotation_angle)
-                else:
-                    rotated_sprite = pygame.transform.rotate(self.sprites['body'], rotation_angle)
-            elif i == 0:
-                rotated_sprite = pygame.transform.rotate(self.sprites['head'], rotation_angle)
-            else:
-                rotated_sprite = pygame.transform.rotate(self.sprites['cauda'], rotation_angle)
-
+            rotated_sprite = pygame.transform.rotate(self.sprites[sprite_type], rotation_angle)
             segment.sprite = rotated_sprite
             segment.draw(window, size, rows)
 
     def collides_with_wall(self, rows):
         head = self.body[0].position
         return head.x < 0 or head.y < 0 or head.x >= rows or head.y >= rows
+
+    def collides_with_self(self):
+        head = self.body[0].position
+        for segment in self.body[1:]:
+            if head.x == segment.position.x and head.y == segment.position.y:
+                return True
+        return False
 
 
 class Game:
@@ -159,13 +169,43 @@ class Game:
                 elif event.key == pygame.K_RIGHT and self.snake.direction != pygame.K_LEFT:
                     self.snake.direction = event.key
 
+    def game_over_screen(self):
+        font = pygame.font.Font(None, 74)
+        game_over = font.render('Game Over', True, (255, 0, 0))
+        retry = font.render('Retry? (y/n)', True, (255, 255, 255))
+
+        self.window.blit(game_over, [self.size // 4, self.size // 3])
+        self.window.blit(retry, [self.size // 4, self.size // 2])
+
+        pygame.display.flip()
+
+        waiting_for_input = True
+        while waiting_for_input:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_y:
+                        print("User chose to retry")
+                        waiting_for_input = False
+                        return True
+                    if event.key == pygame.K_n:
+                        print("User chose to exit")
+                        waiting_for_input = False
+                        return False
+
     def check_collisions(self):
+        # Verifique se a cobra colide com as paredes
         if self.snake.collides_with_wall(self.rows):
-            print("Game Over!")
-            exit()
+            if not self.game_over_screen():
+                exit()
+
+        # Verifique se a cobra colide com ela mesma
+        if self.snake.collides_with_self():
+            if not self.game_over_screen():
+                exit()
+
         if self.snake.body[0].collides_with(self.apple):
             self.apple.position = Position(random.randint(0, self.rows - 1), random.randint(0, self.rows - 1))
-            self.snake.grow()
+            self.snake.should_grow = True
 
     def play(self):
         while True:
@@ -175,14 +215,28 @@ class Game:
             self.check_collisions()
             self.redraw()
 
+            if self.snake.collides_with_wall(self.rows) or self.snake.collides_with_self():
+                retry = self.game_over_screen()
+                print(f"Retry is {retry}")
+                return retry
 
-if __name__ == "__main__":
+
+def main_game():
     sprite_paths = {
         'head': 'img/snake_head.png',
         'body': 'img/snake_body.png',
         'cauda': 'img/snake_cauda.png',
-        'curve_left': 'img/snake_curve_left.png',
+        'curve_left': 'img/snake_curve.png',
         'curve_right': 'img/snake_curve_right.png'
     }
     game = Game(500, 20, sprite_paths)
-    game.play()
+    retry = game.play()
+    print(f"Main game retry is {retry}")
+    return retry
+
+
+if __name__ == "__main__":
+    retry = True
+    while retry:
+        print("Starting a new game")
+        retry = main_game()
